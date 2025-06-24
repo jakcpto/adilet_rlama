c0d20a4db467cc6540a143df9581149fa32e85e2
Jakcpto <potapovev@outlook.com>
2025-06-24 11:47:57 +0400

    some little changes to casual run

--- rlama_create.sh
+++ rlama_create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-curl -fsSL https://raw.githubusercontent.com/dontizi/rlama/main/install.sh | sudo sh
-rlama install-dependencies
-rlama rag hf.co/TECCOD/adilet-llama-8b-250619-ollama adilet ./rlama_docs
+#curl -fsSL https://raw.githubusercontent.com/dontizi/rlama/main/install.sh | sudo sh
+#rlama install-dependencies
+#rlama rag hf.co/TECCOD/adilet-llama-8b-250619-ollama adilet ./rlama_docs
 # for API run next command
 #rlama api adilet
 # for manual tests run this command
